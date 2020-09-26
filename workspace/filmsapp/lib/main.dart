36d3a02256427c4ed261658af69dca4764a3b94b
Enderson Vizcáino <enjaviga@gmail.com>
2020-09-25 22:12:24 -0500

    Modificando el main.dart

--- workspace/filmsapp/lib/main.dart
+++ workspace/filmsapp/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:filmsapp/router/router.dart';
+import 'package:filmsapp/screen/home_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,16 +9,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World :D'),
-          ),
-        ),
-      ),
+      onGenerateRoute: RouterApp.generateRoute,
+      initialRoute: HomeScreen.homePage,
     );
   }
 }
